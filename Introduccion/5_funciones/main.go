d69902b60244bc5ed7c8beb0bcef23a1988e675a
SebastianVeloza <fernando7829@hotmail.com>
2023-01-07 19:19:49 -0500

    funciones recibiendo parametros

--- Introduccion/5_funciones/main.go
+++ Introduccion/5_funciones/main.go
@@ -18,10 +18,27 @@ func sumar(a, b int) int {
 }
 
 func main() {
+	//Llamar las funciones
 	saludar()
 	saludar2("Alex")
 	resultado := sumar(10, 20)
 	fmt.Println("La suma es: ", resultado)
 
+	//******************************************************************************
 	//Ahora recibiendo los parametros de la suma con el scan
+	//Definimos las variabels
+	var a, b, suma int
+
+	//Entrada  de datos
+	fmt.Print("Ingrese N01: ")
+	fmt.Scanln(&a)
+
+	fmt.Print("Ingrese N02: ")
+	fmt.Scanln(&b)
+
+	//Llamar la función sumar nuevamente como es una funcion la podemos usar ilimitadamente
+	suma = sumar(a, b)
+
+	//Salida de datos
+	fmt.Println("La suma: ", a, " + ", b, " = ", suma)
 }
