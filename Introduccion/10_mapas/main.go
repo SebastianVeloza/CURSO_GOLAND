a60624981bb515ccfa1a6f800c988b0773395986
SebastianVeloza <fernando7829@hotmail.com>
2023-01-09 23:49:01 -0500

    map

--- Introduccion/10_mapas/main.go
+++ Introduccion/10_mapas/main.go
@@ -18,4 +18,24 @@ func main() {
 	dias[6] = "Viernes"
 	dias[7] = "Sabado"
 
+	fmt.Println(dias)
+
+	dias[7] = "SABADO"
+
+	fmt.Println(dias)
+
+	delete(dias, 1)
+
+	fmt.Println(dias, len(dias))
+
+	//Nueva Mopa
+	estudiantes := make(map[string][]int)
+
+	estudiantes["Alex"] = []int{13, 15, 16}
+	estudiantes["Roel"] = []int{14, 13, 17}
+
+	fmt.Println(estudiantes)
+
+	fmt.Println(estudiantes["Alex"][1])
+
 }
