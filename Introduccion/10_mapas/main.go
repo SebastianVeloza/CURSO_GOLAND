702dc1834b464813fea611841ff4c9fe5045c8eb
SebastianVeloza <fernando7829@hotmail.com>
2023-01-09 23:48:23 -0500

    make con mapas

---
+++ Introduccion/10_mapas/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	dias := make(map[int]string)
+
+	fmt.Println(dias)
+
+	//Agregar datos
+	dias[1] = "Domingo"
+	dias[2] = "Lunes"
+	dias[3] = "Mates"
+	dias[4] = "Miercoles"
+	dias[5] = "Jueves"
+	dias[6] = "Viernes"
+	dias[7] = "Sabado"
+
+}
