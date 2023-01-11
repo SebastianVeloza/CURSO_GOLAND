6e2a3accc871bdebe98f337fc2b285419249d7c5
SebastianVeloza <fernando7829@hotmail.com>
2023-01-11 18:19:46 -0500

    for each

--- Introduccion/16_for/main.go
+++ Introduccion/16_for/main.go
@@ -43,4 +43,17 @@ func main() {
 		fmt.Println(i)
 	}
 
+	//Bucle For each
+
+	nombres := [...]string{"Alex", "Roel", "Juan"}
+
+	/**
+	for i := 0; i < len(nombres); i++ {
+		fmt.Println(nombres[i])
+	}*/
+
+	for indice, elemento := range nombres {
+		fmt.Println(indice, elemento)
+	}
+
 }
