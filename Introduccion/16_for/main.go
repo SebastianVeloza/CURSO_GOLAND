6428ef8d2f357871a6195d1da5856b3141208c6c
SebastianVeloza <fernando7829@hotmail.com>
2023-01-11 18:15:33 -0500

    Bucle for comun

--- Introduccion/16_for/main.go
+++ Introduccion/16_for/main.go
@@ -1,19 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Nota: En GOLAND NO existe en while pero si se puede usar el for como un while
-
-	//Bucle infinito
 	/*for {
 		fmt.Println("Hola Mundo")
 	}*/
 
 	//Bucle tipo while
-
+	/**
 	numeros := 12455458
 	c := 0
 	for numeros > 0 {
 		numeros /= 10
 		c++
 	}
+	fmt.Println("Cantidad de digitos es: ", c)
+	*/
+
+	//Bucle for
+
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
 }
