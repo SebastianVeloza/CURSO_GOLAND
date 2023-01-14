9cdb376b129e90dd0d260d2ab0f664a3ca1efb29
SebastianVeloza <fernando7829@hotmail.com>
2023-01-14 15:04:35 -0500

    funciones

--- Introduccion/18_funciones/defer-panic-recover/main.go
+++ Introduccion/18_funciones/defer-panic-recover/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -13,4 +14,20 @@ func main() {
 		}
 	}()
 
+	if file, error := os.Open("hola.txt"); error != nil {
+		//Uso de función panic
+		panic("No fue posible óbtener el archivo!!")
+	} else {
+
+		//Uso de defer
+		defer func() {
+			fmt.Println("Cerrando el archivo!")
+			file.Close()
+		}()
+
+		contenido := make([]byte, 254)
+		long, _ := file.Read(contenido)
+		contenidoArchivo := string(contenido[:long])
+		fmt.Println(contenidoArchivo)
+	}
 }
