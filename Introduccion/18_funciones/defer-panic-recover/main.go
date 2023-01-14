3e22e400a5444fe930c111542f9002860e0fe4f7
SebastianVeloza <fernando7829@hotmail.com>
2023-01-14 14:56:21 -0500

    recover

--- Introduccion/18_funciones/defer-panic-recover/main.go
+++ Introduccion/18_funciones/defer-panic-recover/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//Uso de funcion recover
+	defer func() {
+		if error := recover(); error != nil {
+			fmt.Println("Ups!, al parecer el programa no finalizo de forma correcta")
+		}
+	}()
+
+}
